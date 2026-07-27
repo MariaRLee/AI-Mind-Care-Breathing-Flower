844bf700662787c24b670165c785bb873f5bbf2c
MariaRLee <104711840+MariaRLee@users.noreply.github.com>
2026-07-27 15:12:21 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// AI-Mind Care Breathing Flower V1.2.1
-// Paste the deployed Google Apps Script /exec URL below.
-// Leave blank to hide online progress backup.
-window.AI_MIND_CARE_CONFIG = {
-  onlineEndpoint: ""
-};
